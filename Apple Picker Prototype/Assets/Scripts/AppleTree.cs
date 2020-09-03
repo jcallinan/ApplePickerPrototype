5cc494d5187278600d56b7c12b267cd855c8c3ac
jcallinan <jeremy.callinan@gmail.com>
2020-09-03 18:46:17 -0400

    AppleTree.cs

--- Apple Picker Prototype/Assets/Scripts/AppleTree.cs
+++ Apple Picker Prototype/Assets/Scripts/AppleTree.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
+
+    [Header("Set in Inspector")]
+
+    public GameObject applePrefab;
+    public float speed = 1f;
+    public float leftAndRightEdge = 20f;
+    public float chanceToChangeDirections = 0.001f;
+    public float secondsBetweenAppleDrops = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+     //dropping apples every second   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //basic movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+        //changing direction
+
+        if (pos.x < -leftAndRightEdge) {
+            speed = Mathf.Abs(speed);
+        } else if (pos.x > leftAndRightEdge) {
+            speed = -Mathf.Abs(speed);
+        } else if (UnityEngine.Random.value < chanceToChangeDirections ) {
+            speed *= -1;
+        }
     }
 }
